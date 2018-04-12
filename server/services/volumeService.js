d073ba8feaa3d3694acf0c2dc10bba9a68d0ff15
David Sally <davidcsally@gmail.com>
2018-04-11 23:17:11 -0700

    (add) send current volume in response

--- server/services/volumeService.js
+++ server/services/volumeService.js
@@ -3,20 +3,35 @@ exports.volume = (req, res) => {
   const { denon } = res.locals;
   const { volume } = req.params;
   const num = parseInt(volume, 10);
-  denon.command(`MV${num}`);
-  res.end();
+  denon.command(`MV${num}`)
+    .then(() => res.json({ volume: num }));
 };
 
 exports.volumeUp = (req, res) => {
   const { denon } = res.locals;
-  denon.command('MVUP');
-  res.end();
+  denon.command('MVUP')
+    .then((result) => {
+      const numberd = Number(`${result.slice(2, 4)}.${result.slice(4)}`);
+      res.json({ volume: numberd });
+    });
+};
+
+exports.volumeStatus = (req, res) => {
+  const { denon } = res.locals;
+  denon.command('MV?')
+    .then((result) => {
+      const numberd = Number(`${result.slice(2, 4)}.${result.slice(4)}`);
+      res.json({ volume: numberd });
+    });
 };
 
 exports.volumeDown = (req, res) => {
   const { denon } = res.locals;
-  denon.command('MVDOWN');
-  res.end();
+  denon.command('MVDOWN')
+    .then((result) => {
+      const numberd = Number(`${result.slice(2, 4)}.${result.slice(4)}`);
+      res.json({ volume: numberd });
+    });
 };
 
 exports.mute = async (req, res) => {
