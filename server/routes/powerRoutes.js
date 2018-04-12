3a3f22d6ef0e3004f5eb1b739dfc62d87cac5770
David Sally <davidcsally@gmail.com>
2018-04-11 23:16:18 -0700

    (add) status routes

--- server/routes/powerRoutes.js
+++ server/routes/powerRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 
 const router = express.Router();
-const { powerOn, powerOff } = require('../services/powerService');
+const { powerOn, powerOff, powerStatus } = require('../services/powerService');
 
+router.get('/', powerStatus);
 router.post('/on', powerOn);
 router.post('/off', powerOff);
 
--- server/routes/volumeRoutes.js
+++ server/routes/volumeRoutes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const volumeService = require('../services/volumeService');
 
-router.get('/', volumeService.mute);
+router.get('/', volumeService.volumeStatus);
 router.post('/up', volumeService.volumeUp);
 router.post('/down', volumeService.volumeDown);
 router.post('/mute', volumeService.mute);
